ce5071824adab5e93e90194fd760410210a87bb5
Matthias Hannig <matthias@hannig.cc>
2015-10-06 09:40:55 +0200

    Added message serialization

---
+++ message.go
@@ -0,0 +1,62 @@
+package sse
+
+/**
+ * Server Sent Events
+ *
+ * Implement a marshalable Message representing
+ * a SSE.
+ *
+ * (c) 2015 Matthias Hannig
+ */
+
+import (
+	"bytes"
+	"strconv"
+	"strings"
+)
+
+type Message struct {
+	Id    string
+	Event string
+	Data  string
+	Retry int
+}
+
+/**
+ * Serialize message
+ */
+func (m Message) Marshal() ([]byte, error) {
+	var res bytes.Buffer
+
+	// Serialize fields
+	if m.Id != "" {
+		res.WriteString("id: ")
+		res.WriteString(m.Id)
+		res.WriteString("\n")
+	}
+	if m.Event != "" {
+		res.WriteString("event: ")
+		res.WriteString(m.Event)
+		res.WriteString("\n")
+	}
+	if m.Retry != 0 {
+		res.WriteString("retry: ")
+		res.WriteString(strconv.Itoa(m.Retry))
+		res.WriteString("\n")
+	}
+
+	// Split data for linewise serialization
+	lines := strings.Split(m.Data, "\n")
+
+	// Serialize data
+	for _, line := range lines {
+		res.WriteString("data: ")
+		res.WriteString(line)
+		res.WriteString("\n")
+	}
+
+	// Finish message
+	res.WriteString("\n")
+
+	return res.Bytes(), nil
+}
