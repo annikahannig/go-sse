c7781e2495009d32115d0431e111d199c202fdcc
Matthias Hannig <matthias@hannig.cc>
2015-10-22 00:39:08 +0200

    added test case for failing json encoding

--- message_test.go
+++ message_test.go
@@ -1,9 +1,16 @@
 package sse
 
 import (
-	"fmt"
+	"errors"
 	"testing"
 )
+
+// Json encoding error test
+type faulty string
+
+func (f faulty) MarshalJSON() ([]byte, error) {
+	return nil, errors.New("This did not went well.")
+}
 
 // Test Datatype
 type structPayload struct {
@@ -23,19 +30,20 @@ func TestMessageSerialization(t *testing.T) {
 	}
 
 	data, _ := m.MarshalText()
-	fmt.Println("Serialized message (string):")
-	fmt.Println(string(data))
+	t.Log("Serialized message (string):")
+	t.Log(string(data))
 
 	// Serialize raw
 	m = Message{
+		Id:    "foo42",
 		Event: "foo",
 		Retry: 23,
 		Data:  []byte("foo"),
 	}
 
 	data, _ = m.MarshalText()
-	fmt.Println("Serialized message (raw):")
-	fmt.Println(string(data))
+	t.Log("Serialized message (raw):")
+	t.Log(string(data))
 
 	// Use multiline strings
 	m = Message{
@@ -44,8 +52,8 @@ func TestMessageSerialization(t *testing.T) {
 	}
 
 	data, _ = m.MarshalText()
-	fmt.Println("Serialized message (MultilineStringData):")
-	fmt.Println(string(data))
+	t.Log("Serialized message (MultilineStringData):")
+	t.Log(string(data))
 
 	// Serialize complex datatype
 	m = Message{
@@ -57,7 +65,20 @@ func TestMessageSerialization(t *testing.T) {
 	}
 
 	data, _ = m.MarshalText()
-	fmt.Println("Serialized message (struct):")
-	fmt.Println(string(data))
+	t.Log("Serialized message (struct):")
+	t.Log(string(data))
+
+	// Marshaling error
+	m = Message{
+		Event: "foo",
+		Data:  faulty("foo"),
+	}
+
+	data, err := m.MarshalText()
+	t.Log("Serialized message (error):")
+	if err == nil {
+		t.Error("JSON marshalling errors should propagate")
+	}
+	t.Log(err)
 
 }
