66a6336d720ddd06aea3595378dfb3c672fce6a3
Matthias Hannig <matthias@hannig.cc>
2015-10-22 00:29:37 +0200

    improved test coverage

--- message_test.go
+++ message_test.go
@@ -5,6 +5,12 @@ import (
 	"testing"
 )
 
+// Test Datatype
+type structPayload struct {
+	Bar string
+	Baz int
+}
+
 func TestMessageSerialization(t *testing.T) {
 
 	payload := "foo bar"
@@ -17,16 +23,41 @@ func TestMessageSerialization(t *testing.T) {
 	}
 
 	data, _ := m.MarshalText()
-	fmt.Println("Serialized message:")
+	fmt.Println("Serialized message (string):")
 	fmt.Println(string(data))
 
+	// Serialize raw
 	m = Message{
 		Event: "foo",
-		Data:  multilinePayload,
+		Retry: 23,
+		Data:  []byte("foo"),
 	}
 
 	data, _ = m.MarshalText()
-	fmt.Println("Serialized message:")
+	fmt.Println("Serialized message (raw):")
+	fmt.Println(string(data))
+
+	// Use multiline strings
+	m = Message{
+		Event: "foo",
+		Data:  MultilineStringData(multilinePayload),
+	}
+
+	data, _ = m.MarshalText()
+	fmt.Println("Serialized message (MultilineStringData):")
+	fmt.Println(string(data))
+
+	// Serialize complex datatype
+	m = Message{
+		Event: "foo",
+		Data: structPayload{
+			Bar: "baz",
+			Baz: 23,
+		},
+	}
+
+	data, _ = m.MarshalText()
+	fmt.Println("Serialized message (struct):")
 	fmt.Println(string(data))
 
 }
